df7c803e2f5b2e37fea768dc73cbf1765762f8d9
Any Cai <andycai20000212@gmail.com>
2019-02-01 19:45:37 -0800

    First Commit

---
+++ BitcoinTicker/LoadNameVController.swift
@@ -0,0 +1,81 @@
+//
+//  LoadNameVController.swift
+//  BitcoinTicker
+//
+//  Created by Andy Cai on 1/30/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+class LoadNameVController: UIViewController {
+    
+    let defaults = UserDefaults.standard
+    
+    @IBOutlet weak var okButton: UIButton!
+    @IBOutlet weak var enterName: UITextField!
+    
+    @IBAction func onClick(_ sender: Any) {
+        if let text = enterName.text, !text.trimmingCharacters(in: .whitespaces).isEmpty{
+            //Checks if the text is empty or filled with white spaces, if not, proceed
+            saveDataToPlist(object: text) //saves data to Info.plist
+            performSegue(withIdentifier: "goToBitCoin", sender: self) //performs Segue to the
+        }
+        else
+        {
+            //if the space is still empty, prompt the alert
+            alertNoName()
+        }
+    }
+    
+    //Saves username to Info.plist
+    func saveDataToPlist(object: String)
+    {
+        self.defaults.set(object, forKey: "UserName")
+    }
+    
+    //prepares for segue transitioning to the Bitcoin page
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        _ = segue.destination as! ViewController
+    }
+    
+    //MARK: Textfield Delegate
+    // When user press the return key in keyboard, resigns keyboard
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //
+    override func viewDidLoad() {
+        if defaults.string(forKey: "UserName") != nil
+            //Check if the user has already entered their name, if yes, goes to the BitCoin page.
+        {
+            performSegue(withIdentifier: "goToBitCoin", sender: self)
+        }
+        okButton.layer.cornerRadius = 10 //renders the OK button corners round
+        okButton.clipsToBounds = true //same as above
+    }
+    
+    
+    //alert the user that they have not entered a name
+    func alertNoName() {
+        let alert = UIAlertController(title: "No Name Entered!", message: "Please enter your name to enter the next page!", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            switch action.style{
+            case .default:
+                print("default")
+                
+            case .cancel:
+                print("cancel")
+                
+            case .destructive:
+                print("destructive")
+                
+            }}))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
---
+++ BitcoinTicker/ViewController.swift
@@ -0,0 +1,89 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Created by Angela Yu on 23/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let defaults = UserDefaults.standard
+    
+    let apiURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencies = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySymbols = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+    var finalURL = ""
+    var currencyType = ""
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var userGreeting: UITextView!
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currencyPicker.delegate = self //set the currency PickerView's delegate to self
+        currencyPicker.dataSource = self //set the currency Picker's data source to self
+        userGreeting.text = "Hello, "+defaults.string(forKey: "UserName")! //change the greetings sign to "Hello username"
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencies.count;
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencies[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = apiURL+currencies[row] //Get the url to the selected currency row
+        currencyType = currencySymbols[row] //change currency symbol
+        getBitcoinData(url: finalURL) //pull Bitcoin data from API website
+    }
+    
+    
+    
+    //Use Alamofire to request url and get the JSON data from API website
+    func getBitcoinData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+
+                    print("Bitcoin data retrieved!")
+                    let BitcoinJSON : JSON = JSON(response.result.value!)
+
+                    self.updateBitcoinData(json: BitcoinJSON)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
+    
+    
+    //JSON Parsing to update bitcoin data
+    //If data not available, label changes to "Price Unavailable"
+    
+    func updateBitcoinData(json : JSON) {
+        
+        if let bitcoinResult = json["ask"].double {
+            bitcoinPriceLabel.text = String(bitcoinResult)+currencyType
+        } else {
+            bitcoinPriceLabel.text = "Price Unavailable"
+        }
+    }
+//
+}
+
